6b32b48fc15de53e7bca91ee0ca5dd208a7b476b
Bhishan Bhandari <bhishan_0306@deerwalk.edu.np>
2015-01-09 18:00:02 +0545

    Update tests.py

--- whatpageofsearchamion/tests.py
+++ whatpageofsearchamion/tests.py
@@ -1,8 +1,1 @@
-from django.test import TestCase
-class WhatpageofsearchamionTest(TestCase):
-    def test_entered_the_whatpageofsearchamion(self):
-        response = self.client.get('/')
-        self.assertContains(response, "I've entered the Codeship")
-    def test_leads_to_the_whatpageofsearchamion(self):
-        response = self.client.get('/')
-        self.assertContains(response, '<a href="http://www.thetaranights.com">')
+
