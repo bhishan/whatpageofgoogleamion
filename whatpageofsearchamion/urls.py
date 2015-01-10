1b1ae4ef4510cca497590a0c7bef6dc202978fd7
Bhishan Bhandari <bhishan_0306@deerwalk.edu.np>
2015-01-10 22:53:36 +0545

    Update urls.py

--- whatpageofsearchamion/urls.py
+++ whatpageofsearchamion/urls.py
@@ -1,4 +1,4 @@
-#from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns, include, url
+
 
 urlpatterns = patterns('whatpageofsearchamion.views', (r'^$', 'archive'),(r'^create/','create_search'),)
